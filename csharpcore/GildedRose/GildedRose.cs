7df5ed4a49a786a79870830a994fa6cb36274ed3
Antonio DIOMEDE <adiomede@talentia-software.com>
2022-02-18 17:52:20 +0100

    refactoring

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -14,61 +14,42 @@ namespace GildedRoseKata
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+
+                if (isNormalItem(i))
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
+                    DecreaseQualityBy1(i);
                 }
                 else
                 {
                     if (Items[i].Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        IncreaseQualitBy1(i);
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (isBackstagePasses(i))
                         {
                             if (Items[i].SellIn < 11)
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
+                                IncreaseForQualityLessThanLimit(i);
                             }
 
                             if (Items[i].SellIn < 6)
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
+                                IncreaseForQualityLessThanLimit(i);
                             }
                         }
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
+
+                DecreaseSellIn(i);
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (!isAgedBrie(i))
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (!isBackstagePasses(i))
                         {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
+                            DecreaseQuality(i);
                         }
                         else
                         {
@@ -77,13 +58,68 @@ namespace GildedRoseKata
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
+                        IncreaseForQualityLessThanLimit(i);
                     }
                 }
             }
+        }
+
+        private void IncreaseForQualityLessThanLimit(int i)
+        {
+            if (Items[i].Quality < 50)
+            {
+                IncreaseQualitBy1(i);
+            }
+        }
+
+        private void IncreaseQualitBy1(int i)
+        {
+            Items[i].Quality = Items[i].Quality + 1;
+        }
+
+        private void DecreaseQuality(int i)
+        {
+            if (Items[i].Quality > 0)
+            {
+                DecreaseNotSulfurasHandOfRagnaros(i);
+            }
+        }
+
+        private void DecreaseNotSulfurasHandOfRagnaros(int i)
+        {
+            if (!isSulfuras(i))
+            {
+                DecreaseQualityBy1(i);
+            }
+        }
+
+        private void DecreaseQualityBy1(int i)
+        {
+            Items[i].Quality = Items[i].Quality - 1;
+        }
+
+        private void DecreaseSellIn(int i)
+        {
+            if(!isSulfuras(i))
+                Items[i].SellIn = Items[i].SellIn - 1;
+        }
+
+        private bool isAgedBrie(int i)
+        {
+            return Items[i].Name == "Aged Brie";
+        }
+        private bool isBackstagePasses(int i)
+        {
+            return Items[i].Name == "Backstage passes to a TAFKAL80ETC concert";
+        }
+        private bool isSulfuras(int i)
+        {
+            return Items[i].Name == "Sulfuras, Hand of Ragnaros";
+        }
+
+        private bool isNormalItem(int i)
+        {
+            return !isAgedBrie(i) && !isBackstagePasses(i) && !isSulfuras(i);
         }
     }
 }
--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -12,7 +12,32 @@ namespace GildedRoseTests
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal("foo", Items[0].Name);
+        }
+
+
+        [Fact]
+        public void AfterSellDate_test()
+        {
+            IList<Item> Items = new List<Item> {
+                new Item { Name = "foo", SellIn = 0, Quality = 20 }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("foo", Items[0].Name);
+            Assert.Equal(18, Items[0].Quality);
+        }
+
+        [Fact]
+        public void BeforeSellDateNormalItem_test()
+        {
+            IList<Item> Items = new List<Item> {
+                new Item { Name = "foo", SellIn = 1, Quality = 20 }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("foo", Items[0].Name);
+            Assert.Equal(19, Items[0].Quality);
         }
     }
 }
